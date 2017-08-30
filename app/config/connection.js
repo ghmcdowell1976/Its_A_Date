4f460984a0e6fc670185a6abfb8ebc7093648d9a
George McDowell <ghmcdowell@yahoo.com>
2017-08-29 21:22:09 -0400

    First Upload

---
+++ app/config/connection.js
@@ -0,0 +1,16 @@
+var Sequelize = require("sequelize");
+
+// Creates mySQL connection using Sequelize
+var sequelize = new Sequelize("dates_request", "root", "root", {
+  host: "localhost",
+  dialect: "mysql",
+  port: 8889,
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  }
+});
+
+// Exports the connection for other files to use
+module.exports = sequelize;
